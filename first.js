b462567691f250a599885d5116fc314b9970c182
andrew72-max <andrew.nyongesa@student.moringaschool.com>
2024-12-02 15:12:08 +0300

    complete

--- first.js
+++ first.js
@@ -1,29 +1,26 @@
-function getStudentGrade(marks){
-    const studentMarks = 100;
-  
+function checkStudentGrade(studentMarks){
+    
+
 }
 
-const grades = 70;
-
-
-function grading(grades) {
-    if (grades >= 80 && grades <= 100) {
-        console.log("A")
-    } else if (grades >= 60 && grades <= 79) {
-        console.log("B")
-    } else if (grades >= 50 && grades <= 59) {
-        console.log("C")
-    } else if (grades >= 40 && grades <= 49) {
-        console.log("D")
-    } else if (grades < 40) {
+function grading(studentgrade ) {
+    let studentMarks = 100;
+    if (studentgrade >79 ){
+        console.log("A");
+    } else if (studentgrade >=60 && studentgrade  <=79 ){
+        console.log("B");
+    } else if(studentgrade   >=49 && studentgrade  <=59){
+        console.log("C");
+    } else if(studentgrade   >=40 && studentgrade   <= 49){
+        console.log('D');
+    } else if(studentgrade   <40){
         console.log("E")
-    } else {
-        return 'Please enter a valid grade';
+    } else{
+        console.log("please enter a valid grade")
     }
 }
-
-console.log(grading(40)); // Should return 'A'
-console.log(grading(30));
-console.log(grading(50));
-console.log(grading(60));
-console.log(grading(80));
+console.log (grading("80"))//should return A
+console.log (grading("60"))//should return B
+console.log (grading("59"))//should return C
+console.log (grading("40"))//should return D
+console.log (grading("39"))//should return E
